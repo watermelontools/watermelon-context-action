c21919a57562cdaaf83c36c32e60de214cf8e30d
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-06 17:26:38 -0500

    Use axios for request

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-
+const axios = require("axios");
 try {
   // `who-to-greet` input defined in action metadata file
   const time = new Date().toTimeString();
@@ -8,17 +8,11 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
-  fetch("http://app.watermelontools.com/api/jira/getMostRelevantJiraTicket", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({
+  axios
+    .post("http://app.watermelontools.com/api/jira/getMostRelevantJiraTicket", {
       user: "estebandalelr@gmail.com",
       prTitle: "WM-49: Create payments success page",
-    }),
-  })
+    })
     .then((res) => res.json())
     .then((json) => console.log(json));
 } catch (error) {
