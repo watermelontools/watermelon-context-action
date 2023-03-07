fd15803ff3987239f8b5d792de2eda44e3697b6c
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-07 11:13:06 -0500

    Move to typescript

--- getData/jira.ts
+++ getData/jira.ts
@@ -1,12 +1,7 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
 const axios = require("axios");
-try {
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
+export default async function getJira() {
   let textToWrite = "";
-  console.log(`The event payload: ${payload}`);
-  axios
+  await axios
     .post("http://app.watermelontools.com/api/jira/getMostRelevantJiraTicket", {
       user: "estebandalelr@gmail.com",
       prTitle: "WM-49: Create payments success page",
@@ -18,8 +13,9 @@ try {
         jiraText += `\n[${element.key} - ${element.fields.summary}](${element.serverInfo.baseUrl}/browse/${element.key})`;
       }
       textToWrite += jiraText;
-      core.setOutput("textToWrite", textToWrite);
+    })
+    .catch((error) => {
+      console.log(error.message);
     });
-} catch (error) {
-  core.setFailed(error.message);
+  return textToWrite;
 }
---
+++ index.ts
@@ -0,0 +1,19 @@
+import getJira from "./getData/jira";
+
+const core = require("@actions/core");
+const github = require("@actions/github");
+try {
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  let textToWrite = "";
+  console.log(`The event payload: ${payload}`);
+  let getDataPromises = [getJira()];
+  Promise.all(getDataPromises).then((values) => {
+    values.forEach((value) => {
+      textToWrite += value;
+    });
+  });
+  core.setOutput("textToWrite", textToWrite);
+} catch (error) {
+  core.setFailed(error.message);
+}
