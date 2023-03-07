aef1a77ed0f6b2a67eecd96db1e0a13398f691d3
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-06 22:44:35 -0500

    Remove unused code

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const axios = require("axios");
 try {
-  // `who-to-greet` input defined in action metadata file
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
