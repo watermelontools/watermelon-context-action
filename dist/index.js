5d219ce86e981160cb7ecb6002fceb02083321c8
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-08 11:42:18 -0500

    remove buggy code

--- dist/index.js
+++ dist/index.js
@@ -16423,10 +16423,7 @@ const core = __nccwpck_require__(2556);
 const github = __nccwpck_require__(8348);
 try {
     // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github, undefined, 2);
     let textToWrite = "## Context by Watermelon\n";
-    console.log(`The event event: ${payload}`);
-    core.setOutput("issuenumber", github.event.pull_request.number);
     let getDataPromises = [getGithub(), getJira(), getSlack()];
     Promise.all(getDataPromises)
         .then((values) => {
