6a174f9a5b846159ea540f3a1ceda937804ff33d
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-08 11:09:51 -0500

    Change around objects

--- index.ts
+++ index.ts
@@ -6,10 +6,10 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.pull_request, undefined, 2);
+  const payload = JSON.stringify(github.context, undefined, 2);
   let textToWrite = "## Context by Watermelon\n";
   console.log(`The event pull_request: ${payload}`);
-  core.setOutput("issuenumber", github.context.payload.pull_request.number);
+  core.setOutput("issuenumber", github.payload.pull_request.number);
   let getDataPromises = [getGithub(), getJira(), getSlack()];
   Promise.all(getDataPromises)
     .then((values) => {
