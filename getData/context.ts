4b31b21c46e048a99b1687c755399e22bf8d4121
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-14 12:06:04 -0500

    add payload

--- getData/context.ts
+++ getData/context.ts
@@ -6,9 +6,9 @@ export default async function getContext() {
   let textToWrite = "";
   await axios
     .post("http://app.watermelontools.com/api/actions/getContext", {
-      user: github.event.pull_request.user.login,
-      repo: github.event.repository.name,
-      owner: github.event.repository.owner.login,
+      user: github.event.payload.pull_request.user.login,
+      repo: github.event.payload.repository.name,
+      owner: github.event.payload.repository.owner.login,
       commitList: "264ef7c1455b51f1cb65d4457aeaa700478c91f4",
     })
     .then((response) => {
@@ -18,7 +18,7 @@ export default async function getContext() {
       console.log(error.message);
     });
   await axios
-    .get(github.event.pull_request.commits.href)
+    .get(github.event.payload.pull_request.commits.href)
     .then((response) => {
       console.log(response);
     })
