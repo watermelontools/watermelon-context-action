dfd3b815c327a498ab5e4f60a75945569ac96cbd
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-14 12:38:35 -0500

    Fix object

--- getData/context.ts
+++ getData/context.ts
@@ -4,12 +4,11 @@ const github = require("@actions/github");
 const axios = require("axios");
 export default async function getContext() {
   let textToWrite = "";
-  console.log(github);
   await axios
     .post("http://app.watermelontools.com/api/actions/getContext", {
-      user: github.event.payload.pull_request.user.login,
-      repo: github.event.payload.repository.name,
-      owner: github.event.payload.repository.owner.login,
+      user: github.payload.pull_request.user.login,
+      repo: github.payload.repository.name,
+      owner: github.payload.repository.owner.login,
       commitList: "264ef7c1455b51f1cb65d4457aeaa700478c91f4",
     })
     .then((response) => {
@@ -19,7 +18,7 @@ export default async function getContext() {
       console.log(error.message);
     });
   await axios
-    .get(github.event.payload.pull_request.commits.href)
+    .get(github.payload.pull_request.commits.href)
     .then((response) => {
       console.log(response);
     })
