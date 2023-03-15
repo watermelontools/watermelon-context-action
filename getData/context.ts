6fa0bf126589846644be5f7ea456fdf9b7f3355c
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-15 11:58:10 -0500

    Send title and body

--- getData/context.ts
+++ getData/context.ts
@@ -21,6 +21,8 @@ export default async function getContext() {
     repo: github.context.payload.repository.name,
     owner: github.context.payload.repository.owner.login,
     commitList,
+    title: github.context.payload.pull_request.title,
+    body: github.context.payload.pull_request.body,
   });
   console.log(commitList);
 
