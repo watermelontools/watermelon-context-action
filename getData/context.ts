bf50b37e4c912150929e0c281d3aed0f61c82f3f
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-15 13:09:49 -0500

    Add length checking

--- getData/context.ts
+++ getData/context.ts
@@ -41,42 +41,50 @@ export default async function getContext() {
       if (response.data.jiraValue.error === "no jira token") {
         textToWrite += `\n [Click here to login to Jira](https://app.watermelontools.com)`;
       } else {
-        for (let index = 0; index < response.data.jiraValue.length; index++) {
-          const element = response.data.jiraValue[index];
-          textToWrite += `\n - [${element.key} - ${element.fields.summary}](${element.serverInfo.baseUrl}/browse/${element.key})`;
-          textToWrite += `\n`;
-          // shortcircuit to three results
-          if (index === 2) {
-            textToWrite += `and ${response.data.jiraValue.length - 3} more`;
-            break;
+        if (response?.data?.jiraValue?.length) {
+          for (let index = 0; index < response.data.jiraValue.length; index++) {
+            const element = response.data.jiraValue[index];
+            textToWrite += `\n - [${element.key} - ${element.fields.summary}](${element.serverInfo.baseUrl}/browse/${element.key})`;
+            textToWrite += `\n`;
+            // shortcircuit to three results
+            if (index === 2) {
+              textToWrite += `and ${response.data.jiraValue.length - 3} more`;
+              break;
+            }
           }
+        } else {
+          textToWrite += `\n No results found :(`;
         }
       }
       textToWrite += "### Slack Threads";
       if (response.data.slackValue.error === "no slack token") {
         textToWrite += `\n [Click here to login to Slack](https://app.watermelontools.com)`;
       } else {
-        for (
-          let index = 0;
-          index < response.data.slackValue.messages.matches.length;
-          index++
-        ) {
-          const element = response.data.slackValue.messages.matches[index];
-          textToWrite += `\n - [#${element.channel.name} - ${
-            element.username
-          }\n ${
-            element.text.length > 100
-              ? element.text.substring(0, 100) + "..."
-              : element.text
-          }](${element.permalink})`;
-          textToWrite += `\n`;
-          // shortcircuit to three results
-          if (index === 2) {
-            textToWrite += `and ${
-              response.data.slackValue.messages.matches.length - 3
-            } more`;
-            break;
+        if (response?.data?.slackValue?.messages?.matches?.length) {
+          for (
+            let index = 0;
+            index < response.data.slackValue.messages.matches.length;
+            index++
+          ) {
+            const element = response.data.slackValue.messages.matches[index];
+            textToWrite += `\n - [#${element.channel.name} - ${
+              element.username
+            }\n ${
+              element.text.length > 100
+                ? element.text.substring(0, 100) + "..."
+                : element.text
+            }](${element.permalink})`;
+            textToWrite += `\n`;
+            // shortcircuit to three results
+            if (index === 2) {
+              textToWrite += `and ${
+                response.data.slackValue.messages.matches.length - 3
+              } more`;
+              break;
+            }
           }
+        } else {
+          textToWrite += `\n No results found :(`;
         }
       }
     })
