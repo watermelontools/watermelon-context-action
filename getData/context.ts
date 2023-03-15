6fb01b86ce045fbdda42ac1a57296d5acdb0d6dd
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-15 12:02:02 -0500

    Send title and body to backend

--- getData/context.ts
+++ getData/context.ts
@@ -32,6 +32,8 @@ export default async function getContext() {
       repo: github.context.payload.repository.name,
       owner: github.context.payload.repository.owner.login,
       commitList: commitList.toString(),
+      title: github.context.payload.pull_request.title,
+      body: github.context.payload.pull_request.body,
     })
     .then((response) => {
       textToWrite += "### GitHub PRs";
