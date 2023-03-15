6eefec7ef2e46adc25cd1a0f0de9a01cd946f0da
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-14 20:21:49 -0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -16311,38 +16311,40 @@ function getContext() {
     return __awaiter(this, void 0, void 0, function* () {
         let textToWrite = "";
         console.log("url ", github.context.payload.pull_request._links.commits.href);
+        let commitList = [];
         yield axios
             .get(github.context.payload.pull_request._links.commits.href)
             .then((response) => {
-            console.log("commits", response.data);
+            for (let index = 0; index < response.data.length; index++) {
+                commitList.push(response.data[index].sha);
+            }
         })
             .catch((error) => {
             console.log(error.message);
         });
-        /* await axios
-          .post("http://app.watermelontools.com/api/actions/getContext", {
+        yield axios
+            .post("http://app.watermelontools.com/api/actions/getContext", {
             user: github.context.payload.pull_request.user.login,
             repo: github.context.payload.repository.name,
             owner: github.context.payload.repository.owner.login,
-            commitList: "264ef7c1455b51f1cb65d4457aeaa700478c91f4",
-          })
-          .then((response) => {
-            console.log("api", response.data);
+            commitList,
+        })
+            .then((response) => {
             textToWrite += "### GitHub PRs";
             for (let index = 0; index < response.data.items.length; index++) {
-              const element = response.data.items[index];
-              textToWrite += `\n - [#${element.number} - ${element.title}](${element.html_url})`;
-              textToWrite += `\n`;
-              // shortcircuit to three results
-              if (index === 2) {
-                textToWrite += `and ${response.data.items.length - 3} more`;
-                break;
-              }
+                const element = response.data.items[index];
+                textToWrite += `\n - [#${element.number} - ${element.title}](${element.html_url})`;
+                textToWrite += `\n`;
+                // shortcircuit to three results
+                if (index === 2) {
+                    textToWrite += `and ${response.data.items.length - 3} more`;
+                    break;
+                }
             }
-          })
-          .catch((error) => {
+        })
+            .catch((error) => {
             console.log(error.message);
-          }); */
+        });
         return true;
     });
 }
