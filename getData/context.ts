a40b518ab1f58d603e6ac73300a1aadcef7c5d1e
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-14 20:21:39 -0500

    add back commit list

--- getData/context.ts
+++ getData/context.ts
@@ -5,23 +5,25 @@ const axios = require("axios");
 export default async function getContext() {
   let textToWrite = "";
   console.log("url ", github.context.payload.pull_request._links.commits.href);
+  let commitList = [];
   await axios
     .get(github.context.payload.pull_request._links.commits.href)
     .then((response) => {
-      console.log("commits", response.data);
+      for (let index = 0; index < response.data.length; index++) {
+        commitList.push(response.data[index].sha);
+      }
     })
     .catch((error) => {
       console.log(error.message);
     });
-  /* await axios
+  await axios
     .post("http://app.watermelontools.com/api/actions/getContext", {
       user: github.context.payload.pull_request.user.login,
       repo: github.context.payload.repository.name,
       owner: github.context.payload.repository.owner.login,
-      commitList: "264ef7c1455b51f1cb65d4457aeaa700478c91f4",
+      commitList,
     })
     .then((response) => {
-      console.log("api", response.data);
       textToWrite += "### GitHub PRs";
       for (let index = 0; index < response.data.items.length; index++) {
         const element = response.data.items[index];
@@ -36,7 +38,7 @@ export default async function getContext() {
     })
     .catch((error) => {
       console.log(error.message);
-    }); */
+    });
 
   return true;
 }
