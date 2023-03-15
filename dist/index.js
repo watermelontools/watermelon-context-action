e3982352852f66a60c618265ae2d8196073fe6c9
Esteban Dalel R <estebandalelr@gmail.com>
2023-03-14 20:05:26 -0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -16310,6 +16310,7 @@ const axios = __nccwpck_require__(4158);
 function getContext() {
     return __awaiter(this, void 0, void 0, function* () {
         let textToWrite = "";
+        console.log(github.context.payload.pull_request.commits.href);
         yield axios
             .get(github.context.payload.pull_request.commits.href)
             .then((response) => {
@@ -16318,30 +16319,30 @@ function getContext() {
             .catch((error) => {
             console.log(error.message);
         });
-        yield axios
-            .post("http://app.watermelontools.com/api/actions/getContext", {
+        /* await axios
+          .post("http://app.watermelontools.com/api/actions/getContext", {
             user: github.context.payload.pull_request.user.login,
             repo: github.context.payload.repository.name,
             owner: github.context.payload.repository.owner.login,
             commitList: "264ef7c1455b51f1cb65d4457aeaa700478c91f4",
-        })
-            .then((response) => {
+          })
+          .then((response) => {
             console.log("api", response.data);
             textToWrite += "### GitHub PRs";
             for (let index = 0; index < response.data.items.length; index++) {
-                const element = response.data.items[index];
-                textToWrite += `\n - [#${element.number} - ${element.title}](${element.html_url})`;
-                textToWrite += `\n`;
-                // shortcircuit to three results
-                if (index === 2) {
-                    textToWrite += `and ${response.data.items.length - 3} more`;
-                    break;
-                }
+              const element = response.data.items[index];
+              textToWrite += `\n - [#${element.number} - ${element.title}](${element.html_url})`;
+              textToWrite += `\n`;
+              // shortcircuit to three results
+              if (index === 2) {
+                textToWrite += `and ${response.data.items.length - 3} more`;
+                break;
+              }
             }
-        })
-            .catch((error) => {
+          })
+          .catch((error) => {
             console.log(error.message);
-        });
+          }); */
         return true;
     });
 }
